0ea23cd840fd1db19728fa46536e5ea4375bd039
riteshramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-08-16 12:02:01 +0100

    Moved javascript from view to separate file in assets/scripts

---
+++ src/NetC.JuniorDeveloperExam.Web/Assets/Scripts/emailValidation.js
@@ -0,0 +1,36 @@
+﻿//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 1000;  //time in ms (5 seconds)
+
+//on keyup, start the countdown
+$('#emailAddress').keyup(function () {
+    clearTimeout(typingTimer);
+    if ($('#emailAddress').val()) {
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    }
+});
+
+//user is "finished typing," do something
+function doneTyping() {
+    console.log($('#emailAddress').val())
+    $.ajax({
+        url: '/checkEmail',
+        type: 'POST',
+        data: { emailAddress: $('#emailAddress').val() },
+        success: function (validEmailAddress) {
+            console.log(validEmailAddress);
+            if (validEmailAddress == "False") {
+                $('#emailMessage').css('display', 'block');
+                $('#submitCommentBtn').addClass('btn-secondary');
+                $('#submitCommentBtn').attr('disabled', 'true');
+            } else {
+                $('#emailMessage').css('display', 'none');
+                $('#submitCommentBtn').removeClass('btn-secondary');
+                $('#submitCommentBtn').removeAttr('disabled');
+            }
+        },
+        error: function (error) {
+            alert(error);
+        }
+    });
+}
